3905533179bf6e7241fe726ddf0722c2ae932a42
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2021-12-05 23:46:51 +0530

    Create frequency.java

---
+++ frequency.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class fr_str
+{ 
+ public static void main(String args[])
+  { 
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter the string: ");
+   String s=sc.nextLine();
+   String s1=s.toLowerCase();
+   System.out.println("Enter the character whose frequency is to be determined  : ");
+   char ch=sc.next().charAt(0);
+   int f=0
+   for(int i=0;i<s.length();i++)
+   { 
+    if(ch==str.charAt(i))
+     f++;
+   }
+   System.out.println("The frequency of "+ch+" = "+freq);
+  }
+}
