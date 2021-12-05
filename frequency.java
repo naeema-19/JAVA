147e8be0225550325a498e0dada422e16d881c1c
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2021-12-06 00:27:37 +0530

    Update frequency.java

--- frequency.java
+++ frequency.java
@@ -9,12 +9,12 @@ class fr_str
    String s1=s.toLowerCase();
    System.out.println("Enter the character whose frequency is to be determined  : ");
    char ch=sc.next().charAt(0);
-   int f=0
-   for(int i=0;i<s.length();i++)
+   int f=0;
+   for(int i=0;i<s1.length();i++)
    { 
-    if(ch==str.charAt(i))
+    if(ch==s1.charAt(i))
      f++;
    }
-   System.out.println("The frequency of "+ch+" = "+freq);
+   System.out.println("The frequency of "+ch+" = "+f);
   }
 }
