cff8994f75e60baff0c5529b3afb6faf9ed106d7
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2022-01-20 11:36:02 +0530

    Create fileread_write.java

---
+++ fileread_write.java
@@ -0,0 +1,27 @@
+/*.Write a Java program that read from a file and write to file by handling all file related exceptions.(Using fileInput and fileOutput stream class)*/
+
+import java.io.*;
+public class fileread_write{
+
+   public static void main(String args[]) throws IOException {  
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+         in = new FileInputStream("input.txt");
+         out = new FileOutputStream("output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+}
