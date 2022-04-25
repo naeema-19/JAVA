9fb42dc420fd9220af2787e83580f42a5cdce57c
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2022-04-26 00:52:10 +0530

    Update string_tokenizer.java

--- string_tokenizer.java
+++ string_tokenizer.java
@@ -1,28 +1,26 @@
 /* Write a JAVA program that reads a line of integers and then displays each integer, and the sum of all the integers (use String Tokenizer class of java.util)*/
 
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+public class Main {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int dig=0,sum=0; 
+		System.out.println("Enter the digits with space in between them");
+		String s1 = sc.nextLine();
+		StringTokenizer token = new StringTokenizer(s1);
+		System.out.println("Entered Numbers");
+		while(token.hasMoreTokens())
+		{
+			String s3 = token.nextToken();
+			dig = Integer.parseInt(s3);
+			System.out.println(dig + " ");
+			sum = sum + dig;
+			
+		}
+		System.out.println("\n Sum = " + sum);
+		
+		
+	}
 
-class string_tokenizer
-{
-            public static void main(String args[])
-            {
-                        Scanner scr=new Scanner(System.in);
-System.out.println("\nEnter sequence of integers with space b/w them and press enter : ");
-                        String digit=scr.nextLine();
-                        StringTokenizer token=new StringTokenizer(digit);
-                        int dig=0,sum=0;
-                        System.out.println("\nEntered digits are : ");
-                        while(token.hasMoreTokens())
-                        {
-                                    String s=token.nextToken();
-                                    dig=Integer.parseInt(s);
-                                    System.out.print(dig+" ");
-                                    sum=sum+dig;
-                        }
-                        System.out.println();
-                        System.out.println("Sum is : "+sum);
-            }
 }
